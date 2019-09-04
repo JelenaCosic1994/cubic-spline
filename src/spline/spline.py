ad76f7f210e60662ef73719a32bf6bbeb8732ff7
JelenaCosic1994 <jelenacosic94@gmail.com>
2019-09-04 22:38:30 +0200

    cubic polynomial instead of function

--- src/spline/spline.py
+++ src/spline/spline.py
@@ -2,9 +2,9 @@ class Spline:
 
     def __init__(self):
         self._ranges = []
-        self._functions = []
+        self._cubic_polynomials = []
 
-    def add_function_for_range(self, function, x1, x2):
+    def add_cubic_polynomial_for_range(self, cubic_polynomial, x1, x2):
         self._ranges.append((x1, x2))
-        self._functions.append(function)
+        self._cubic_polynomials.append(cubic_polynomial)
 
