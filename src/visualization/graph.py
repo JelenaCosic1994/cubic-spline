c1d36a951a2954046cbf163d2f1a58048c5401cc
JelenaCosic <jelenacosic94@gmail.com>
2019-09-11 18:58:04 +0200

    unnecessary import removed and variable renamed

--- src/visualization/graph.py
+++ src/visualization/graph.py
@@ -2,8 +2,6 @@ import matplotlib.pyplot as plt
 import numpy
 import copy
 
-from src.spline.spline import Spline
-
 STEP = 0.01
 
 
@@ -11,9 +9,9 @@ def plot_spline(spline):
     array_for_plotting = []
     for interval_and_function in spline.get_ranges_and_functions():
         interval = interval_and_function['range']
-        function = interval_and_function['function']
+        function_on_interval = interval_and_function['function']
         x = numpy.arange(interval[0], interval[1] + STEP, STEP)
         array_for_plotting.append(copy.deepcopy(x))
-        array_for_plotting.append(function(array_for_plotting[-1]))
+        array_for_plotting.append(function_on_interval(array_for_plotting[-1]))
     plt.plot(*array_for_plotting)
     plt.show()
