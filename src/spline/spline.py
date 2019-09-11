40742ace1d618fc0d07b9ca02d37b399b97d10b7
JelenaCosic <jelenacosic94@gmail.com>
2019-09-11 19:04:15 +0200

    comments added to spline

--- src/spline/spline.py
+++ src/spline/spline.py
@@ -8,9 +8,19 @@ class Spline:
         self._cubic_polynomials = []
 
     def add_cubic_polynomial_for_range(self, coefficients, x1, x2):
+        """
+        This method adds cubic polynomial specified via coefficients (a0, a1, a2, a3) to specific range
+        :param coefficients: array([a0, a1, a2, a3]) used for cubic polynomial creation
+        :param x1: range lower bound
+        :param x2: range upper bound
+        """
         self._ranges.append((x1, x2))
         self._cubic_polynomials.append(CubicPolynomial(*coefficients))
 
     def get_ranges_and_functions(self):
+        """
+        :return: List of pairs(dictionaries): range, function. Range is specified as tuple
+        e.g. (lower_bound, upper_bound)
+        """
         return [{'range': self._ranges[i], 'function': self._cubic_polynomials[i].get_function()}
                 for i in range(len(self._ranges))]
