93cdb0bafb053b6821e1259e2d0cd23c69178a01
JelenaCosic <jelenacosic94@gmail.com>
2019-09-11 22:11:30 +0200

    periodic spline completed

--- src/creator/periodic.py
+++ src/creator/periodic.py
@@ -15,22 +15,22 @@ class PeriodicSplineCreator(BaseSplineCreator):
         A = np.zeros((n, n))  # matrica koja ce biti matrica sistema za nalazenje momentata
         B = np.zeros(n)  # vektor koji ce sadrzati slobodne koeficijente sistema
 
-        # granicni uslovi za ovaj tip splajna
+        # granicni uslovi za ovaj tip splajna, ni[0] je u stvari ni_1, mi[0] je u stvari mi_1
         A[0] = np.hstack((np.array((2, ni[0])), np.zeros((n - 3)), np.array((mi[0]))))
 
-        B[-1] = 6 / (h[-1] + h[1]) * ((f[1] - f[-1]) / h[1] - (f[-1] - f[-2]) / h[-1])
+        for i in range(1, n-1):  # od 1 do n-2 (ima ih n - 2)
+            A[i] = np.hstack((np.zeros((i - 1)), np.array((mi[i], 2, ni[i])), np.zeros((n - i - 1))))
+
+        ni_n = h[1]/(h[-1] + h[1])
+        mi_n = 1 - ni_n
+
+        A[-1] = np.hstack((ni_n, np.zeros((n - 3)), np.array((mi_n, 2))))
 
         # sve ostale jednacine, osim prve i poslednje
         for i in range(0, n - 1):  # od 0 do n-2 (ima ih n - 1)
             B[i] = lambd[i]  # ovde upisujemo lambda1, lambda2,.. lambda(n-1)
 
-        # dovde je odradjeno, u ni i mi treba ubaciti poslednje clanove po formuli za ovaj tip
-        # u mi i ni nemamo mi_0 i mi_n imamo samo od 1 do n-1 elemente
-
-        A[-1] = np.hstack((np.array((ni[-1])), np.zeros((n - 3)), np.array((mi[-1], 2))))
-
-        for i in range(1, n - 1):  # od 1 do n-2 (ima ih n - 2)
-            A[i] = np.hstack((np.zeros((i - 1)), np.array((mi[i - 1], 2, ni[i - 1])), np.zeros((n - i - 1))))
+        B[-1] = 6 / (h[-1] + h[1]) * ((f[1] - f[-1]) / h[1] - (f[-1] - f[-2]) / h[-1])
 
         print("A=", A)
         print("B=", B)
