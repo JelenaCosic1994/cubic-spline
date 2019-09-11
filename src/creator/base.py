906e2c7fa9555200840f8bc2882e0b1bad6b37fc
JelenaCosic <jelenacosic94@gmail.com>
2019-09-12 00:15:14 +0200

    cubic_spline method devided into several methods

--- src/creator/base.py
+++ src/creator/base.py
@@ -10,36 +10,31 @@ class BaseSplineCreator(ABC):
     def get_m(self, **kwargs):
         pass
 
-    def cubic_spline(self, t, **kwargs):
+    @staticmethod
+    def calculate_raw_spline(alpha, beta, gamma, delta, x, n):
+        raw_spline = []
+        for i in range(0, n):
+            s1 = np.hstack((np.zeros(n+1), np.array(alpha[i])))
+            s2 = np.hstack((np.zeros(n), np.array((beta[i], -beta[i] * x[i]))))
+            s3 = np.hstack((np.zeros(n - 1), np.array((gamma[i], -2 * gamma[i] * x[i], gamma[i] * x[i] ** 2))))
+            s4 = np.hstack((np.zeros(n - 2), np.array((delta[i], -3 * delta[i] * x[i], 3 * delta[i] * x[i] ** 2,
+                                                       -delta[i] * math.pow(x[i], 3)))))
 
-        x = t[0, :]  # (ima duzinu n + 1) x0, x1, x2,.. xn
-        f = t[1, :]  # (ima duzinu n + 1) x0, x1, x2,.. xn
-        h = [None] + list(np.diff(x))  # rastojanja izmedju cvorova (ima duzinu n + 1), nulti element se ne koristi,
-        #  dodat zbog lakseg indeksiranja
+            raw_spline.append(s1 + s2 + s3 + s4)
+        return raw_spline
 
-        n = len(x) - 1  # x0, x1,.. xn (n je n iz indeksa xn)
+    @staticmethod
+    def convert_raw_spline_into_spline(raw_spline, x, n):
+        spline = Spline()
+        for i in range(0, n):
+            x1 = x[i]
+            x2 = x[i+1]
+            spline_coefficients = raw_spline[i][-4:]
+            spline.add_cubic_polynomial_for_range(spline_coefficients, x1, x2)
+        return spline
 
-        ni = [None] * n  # (ima duzinu n) nulti element dodat zbog lakseg indeksiranja
-        mi = [None] * n  # (ima duzinu n) nulti element dodat zbog lakseg indeksiranja
-        lambd = [None] * n  # (ima duzinu n) nulti element dodat zbog lakseg indeksiranja
-
-        for i in range(1, n):  # 1, 2,.. n-1
-            ni[i] = h[i+1] / (h[i] + h[i+1])
-            mi[i] = 1 - ni[i]
-            lambd[i] = (6/(h[i] + h[i+1]))*((f[i+1] - f[i])/h[i+1] - (f[i] - f[i-1])/h[i])
-
-        print("Mi: ", mi)
-        print("Ni: ", ni)
-        print("Lambda: ", lambd)
-        mi = mi[1:]
-        ni = ni[1:]
-        lambd = lambd[1:]
-
-        M = self.get_m(n=n, h=h, f=f, ni=ni, mi=mi, lambd=lambd, **kwargs)
-
-        print("M: ", M)
-
-        # sad trazimo splajn na pojedinacnim intervalima; prvo racunamo a, b, c, i d
+    @staticmethod
+    def calculate_coefficients(h, f, M, n):
         alpha = []
         beta = []
         gamma = []
@@ -50,30 +45,39 @@ class BaseSplineCreator(ABC):
             gamma.append(1 / 2 * M[i])
             delta.append((1 / (6 * h[i+1])) * (M[i + 1] - M[i]))
 
-        print("alpha = ", alpha)
-        print("beta = ", beta)
-        print("gamma = ", gamma)
-        print("delta = ", delta)
-    #
-        # sad racunamo splajn
-        S = []
-        for i in range(0, n):
-            S1 = np.hstack((np.zeros(n+1), np.array(alpha[i])))
-            S2 = np.hstack((np.zeros(n), np.array((beta[i], -beta[i] * x[i]))))
-            S3 = np.hstack((np.zeros(n - 1), np.array((gamma[i], -2 * gamma[i] * x[i], gamma[i] * x[i] ** 2))))
-            S4 = np.hstack((np.zeros(n - 2), np.array((delta[i], -3 * delta[i] * x[i], 3 * delta[i] * x[i] ** 2, -delta[i] * math.pow(x[i], 3)))))
+        return alpha, beta, gamma, delta
 
-            S.append(S1 + S2 + S3 + S4)
+    @staticmethod
+    def calculate_ni_mi_lambda(f, h, n):
+        ni = [None] * n  # (ima duzinu n) nulti element dodat zbog lakseg indeksiranja
+        mi = [None] * n  # (ima duzinu n) nulti element dodat zbog lakseg indeksiranja
+        lambd = [None] * n  # (ima duzinu n) nulti element dodat zbog lakseg indeksiranja
 
-        print("Ceo splajn: ", S)
+        for i in range(1, n):  # 1, 2,.. n-1
+            ni[i] = h[i+1] / (h[i] + h[i+1])
+            mi[i] = 1 - ni[i]
+            lambd[i] = (6/(h[i] + h[i+1]))*((f[i+1] - f[i])/h[i+1] - (f[i] - f[i-1])/h[i])
 
-        spline = Spline()
+        mi = mi[1:]
+        ni = ni[1:]
+        lambd = lambd[1:]
 
-        for i in range(0, n):
-            x1 = x[i]
-            x2 = x[i+1]
-            spline_coefs = S[i][-4:]
+        return ni, mi, lambd
 
-            spline.add_cubic_polynomial_for_range(spline_coefs, x1, x2)
+    def cubic_spline(self, t, **kwargs):
 
-        return spline
+        x = t[0, :]  # (ima duzinu n + 1) x0, x1, x2,.. xn
+        f = t[1, :]  # (ima duzinu n + 1) x0, x1, x2,.. xn
+        h = [None] + list(np.diff(x))  # rastojanja izmedju cvorova (ima duzinu n + 1), nulti element se ne koristi,
+        #  dodat zbog lakseg indeksiranja
+
+        n = len(x) - 1  # x0, x1,.. xn (n je n iz indeksa xn)
+
+        ni, mi, lambd = BaseSplineCreator.calculate_ni_mi_lambda(f, h, n)
+
+        M = self.get_m(n=n, h=h, f=f, ni=ni, mi=mi, lambd=lambd, **kwargs)
+
+        alpha, beta, gamma, delta = BaseSplineCreator.calculate_coefficients(h, f, M, n)
+
+        return BaseSplineCreator.convert_raw_spline_into_spline(
+            BaseSplineCreator.calculate_raw_spline(alpha, beta, gamma, delta, x, n), x, n)
